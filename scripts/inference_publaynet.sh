cdff3631e0fe4f45de1b5ef928ae05fa32a49286
jjihwan <kjh26720@snu.ac.kr>
2024-06-16 02:58:37 +0900

    first commit

---
+++ scripts/inference_publaynet.sh
@@ -0,0 +1,9 @@
+python inference_c.py \
+        --dataset publaynet \
+        --experiment c \
+        --device cuda:0 \
+        --dim_transformer 1024 \
+        --nhead 16 \
+        --batch_size 2048 \
+        --beautify \
+        --plot
---
+++ scripts/inference_rico.sh
@@ -0,0 +1,9 @@
+python inference_c.py \
+        --dataset rico25 \
+        --experiment c \
+        --device cuda:0 \
+        --dim_transformer 512 \
+        --nhead 16 \
+        --batch_size 2048 \
+        --beautify \
+        --plot
